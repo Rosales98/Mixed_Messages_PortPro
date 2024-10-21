bfd7be1b97d1e23b5d4f9043c79c34271422e342
Claudio <claudio.cruzrosales@gmail.com>
2024-10-21 01:06:05 -0400

    added characters, location and challange

---
+++ script.js
@@ -0,0 +1,3 @@
+const characters = ["Brave Knight", "Clever Thief", "Mystical Wizard", "Fearless Archer"];
+const location = ["Dark Forrest", "Ancient Ruins", "Mountain Peak", "Enchanted Castle"];
+const challange = ["Face a fierce dragon", "Solve a mystical riddle", "Cross a dangerous river", "Rescue the lost prince"];
