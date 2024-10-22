cfa70f104a929a3bb13f72c3bac22a5b8d22b30c
Claudio <claudio.cruzrosales@gmail.com>
2024-10-22 02:25:46 -0400

    made a function to randomize

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const characters = ["Brave Knight", "Clever Thief", "Mystical Wizard", "Fearless Archer"];
-const location = ["Dark Forrest", "Ancient Ruins", "Mountain Peak", "Enchanted Castle"];
-const challange = ["Face a fierce dragon", "Solve a mystical riddle", "Cross a dangerous river", "Rescue the lost prince"];
+const locations = ["Dark Forrest", "Ancient Ruins", "Mountain Peak", "Enchanted Castle"];
+const challanges = ["Face a fierce dragon", "Solve a mystical riddle", "Cross a dangerous river", "Rescue the lost prince"];
+
+function getRandomMessage(arr) {
+    return arr[Math.floor(Math.random()*arr.length)];
+}
+
+
